8de4c82db5dd4186f310982b0918e614edb8bf9c
Gurinder Singh <gurindersingh.dev@gmail.com>
2021-05-25 19:03:17 +0530

    ui bootstrap auth added

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
